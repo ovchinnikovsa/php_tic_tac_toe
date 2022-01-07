99b1876f75440b47b166794d3e296a571d5b25db
ovchie <fast.rem1ck3@gmail.com>
2022-01-07 20:22:13 +0300

    first

---
+++ view/pages/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/view/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/view/assets/css/style.css">
+    <title>Tic tac toe</title>
+</head>
+
+<header>
+    <div class="container-sm">
+        <div class="row justify-content-center">
+            <h1 class="header-title">Tic tac toe</h1>
+        </div>
+    </div>
+</header>
+
+<body>
+<div class="container-sm">
+    <div class="row justify-content-center">
+        <div class="cells">
+            <button type="submit" class="cell">X</button>
+            <button type="submit" class="cell">X</button>
+            <button type="submit" class="cell">X</button>
+            <button type="submit" class="cell">X</button>
+            <button type="submit" class="cell">X</button>
+            <button type="submit" class="cell">X</button>
+            <button type="submit" class="cell">X</button>
+            <button type="submit" class="cell">X</button>
+            <button type="submit" class="cell">X</button>
+        </div>
+    </div>
+</div>
+</body>
+
+<script src="/view/assets/js/bootstrap.min.js"></script>
+
+</html>
